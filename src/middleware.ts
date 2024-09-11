9014c650bd76146c817d588e7c01c38d4c6362ae
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-11 10:47:03 +0200

    Add Github as an Auth Provider

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,21 @@ import {
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
 
-const isPublicPage = createRouteMatcher(["/signin"]);
+const isSignInPage = createRouteMatcher(["/auth"]);
+const isProtectedRoute = createRouteMatcher(["/product(.*)"]);
 
-export default convexAuthNextjsMiddleware();
+// Configure which routes require authentication
+export default convexAuthNextjsMiddleware((request) => {
+  if (isSignInPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/product");
+  }
+  if (isProtectedRoute(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+  if (!isSignInPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+});
 
 export const config = {
   // The following matcher runs middleware on all routes
