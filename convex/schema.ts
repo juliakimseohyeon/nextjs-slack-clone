7a2d3fc503040c8168593b3dbb5bc8c243532333
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-23 19:04:43 +0200

    Create new table schema called workspaces

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,12 @@ import { authTables } from "@convex-dev/auth/server";
 
 const schema = defineSchema({
   ...authTables,
-  // Your other tables...
+
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joinCode: v.string(),
+  }),
 });
 
 export default schema;
