e738535efc52fa3b4a90150c0fd9beb452a138ff
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-23 20:23:34 +0200

    Prevent potential hydration error by creating hooks to only render client components when server is loaded

--- src/components/ui/Modals.tsx
+++ src/components/ui/Modals.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
 
 export const Modals = () => {
+  // Hooks to prevent hydration error by only rendering client components when server is loaded
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
       <CreateWorkspaceModal />
