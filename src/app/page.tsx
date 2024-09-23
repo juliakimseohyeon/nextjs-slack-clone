615ef53aa6bf5d1719f4c8567d99e89952ded194
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-23 20:07:12 +0200

    Create file to handle workspace modal to open and close, and import it into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+
 export default function Home() {
+  const [modalOpen, setModalOpen] = useCreateWorkspaceModal();
+  const { workspaces, isLoading } = useGetWorkspaces();
+
+  const workspaceId = useMemo(() => workspaces?.[0]?._id, [workspaces]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      console.log("Redirect to workspace");
+    } else if (!modalOpen) {
+      setModalOpen(true);
+    }
+  }, [workspaceId, isLoading, modalOpen, setModalOpen]);
+
   return (
     <div>
       <UserButton />
