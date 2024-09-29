27078b6266a3ed38b7427cb5ba153df41e1b73f4
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-29 15:43:06 +0200

    Create new API Route in workspaces table to get the specific workspace

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
+  const { workspace } = useGetWorkspace({ id: workspaceId });
 
-  return <div>ID: {workspaceId}</div>;
+  return <div>ID: {JSON.stringify(workspace)}</div>;
 };
 
 export default WorkspaceIdPage;
