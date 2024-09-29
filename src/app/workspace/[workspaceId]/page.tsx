c0939d8bc5e499d003c07f14256d8f1e1c96b2cf
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-29 16:40:07 +0200

    Clean up content in WorkspaceIdPage

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { workspace } = useGetWorkspace({ id: workspaceId });
-
-  return <div>ID: {JSON.stringify(workspace)}</div>;
+  return <div>Workspace Id Page</div>;
 };
 
 export default WorkspaceIdPage;
