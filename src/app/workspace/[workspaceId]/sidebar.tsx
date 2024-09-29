440616abb9d0af6c0973b7ea3d5e9e88f17a0c60
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-29 16:53:53 +0200

    Create SidebarButton component with icons that would redirect users to different pages within the app

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,10 +1,30 @@
+import { usePathname } from "next/navigation";
+
 import { UserButton } from "@/features/auth/components/user-button";
 import { WorkspaceSwitcher } from "./workspace-switcher";
 
+import { SidebarButton } from "./sidebar-button";
+import {
+  BellIcon,
+  Home,
+  MessagesSquareIcon,
+  MoreHorizontalIcon,
+} from "lucide-react";
+
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="w-[70px] h-full bg-purple flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessagesSquareIcon} label="DMs" />
+      <SidebarButton icon={BellIcon} label="Activity" />
+      <SidebarButton icon={MoreHorizontalIcon} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
