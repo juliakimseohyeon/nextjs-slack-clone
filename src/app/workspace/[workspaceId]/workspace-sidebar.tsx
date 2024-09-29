f9abddafd98575fafd175432c2fa7358f83b348f
Julia Kim <juliakimseohyeon@gmail.com>
2024-09-29 20:16:20 +0200

    Create skeleton for WorkspaceSidebar component

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,3 +1,36 @@
+import { useCurrentMember } from "@/features/members/api/user-current-members";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { AlertTriangle, Loader } from "lucide-react";
+
 export const WorkspaceSidebar = () => {
-  return <div>Workspace Sidebar</div>;
+  const workspaceId = useWorkspaceId();
+
+  const { currentMember, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+
+  if (workspaceLoading || memberLoading) {
+    return (
+      <div className="flex flex-col bg-lightpurple h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-white" />
+      </div>
+    );
+  }
+
+  if (!workspace || !currentMember) {
+    return (
+      <div className="flex flex-col gap-y-2 bg-lightpurple h-full items-center justify-center">
+        <AlertTriangle />
+        <p className="text-white text-sm">Workspace not found</p>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col bg-lightpurple h-full">Workspace Header</div>
+  );
 };
